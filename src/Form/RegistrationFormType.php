7f4218ed623dc991e0ceb9946776ef4141b040eb
axelpm4 <paulinomarquezaxel@gmail.com>
2026-05-26 10:17:34 +0200

    `Updated margin-bottom of label[for="registration_form_fax_number"] from 5px to 6px`

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,21 +5,33 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Blank;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            // 1. Email avec contraintes strictes
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email',
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez renseigner une adresse email.']),
+                    new Email(['message' => 'L\'adresse email "{{ value }}" n\'est pas valide.']),
+                ],
+            ])
             
+            // 2. Nom d'utilisateur
             ->add('username', TextType::class, [
                 'label' => "Nom d'utilisateur",
                 'constraints' => [
@@ -29,31 +41,58 @@ class RegistrationFormType extends AbstractType
                     new Length([
                         'min' => 3,
                         'minMessage' => "Votre nom d'utilisateur doit faire au moins {{ limit }} caractères",
-                        'max' => 255,
+                        'max' => 50,
                     ]),
                 ],
             ])
             
+            // 3. Case à cocher CGU (Traduit en Français)
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => "J'accepte les conditions d'utilisation du site",
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter nos conditions d\'utilisation pour vous inscrire.',
                     ]),
                 ],
             ])
             
+            // 4. Mot de passe ultra-sécurisé avec REGEX
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit faire au moins {{ limit }} caractères',
                         'max' => 4096,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+                        'message' => 'Votre mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial (@$!%*?&).',
+                    ]),
+                ],
+            ])
+
+            // 5. LE CAPTCHA invisible (Honeypot)
+            // On ajoute un champ alléchant pour un robot (un faux numéro de fax)
+            ->add('fax_number', TextType::class, [
+                'mapped' => false,
+                'required' => false,
+                'label' => false,
+                'attr' => [
+                    'style' => 'display:none !important;', // Caché aux yeux des humains
+                    'tabindex' => '-1',                     // Empêche la navigation au clavier dessus
+                    'autocomplete' => 'off'
+                ],
+                'constraints' => [
+                    // Si ce champ contient QUOIQUE CE SOIT, la validation échoue automatiquement
+                    new Blank([
+                        'message' => 'Une activité suspecte a été détectée (Robot). Il faut laisser ce champ vide.',
                     ]),
                 ],
             ])
@@ -64,6 +103,10 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            // Activation explicite de la protection CSRF
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id'   => 'registration_item',
         ]);
     }
 }
